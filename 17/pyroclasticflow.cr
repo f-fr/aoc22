900d90e3d2c74e4ac336ded2c056778dbaeb2ef9
Frank Fischer <frank-fischer@shadow-soft.de>
2022-12-17 18:09:48 +0100

    17: add simple, direct cycle detection

--- 17/pyroclasticflow.cr
+++ 17/pyroclasticflow.cr
@@ -15,56 +15,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see  <http://www.gnu.org/licenses/>
 
+N = 1_000_000_000_000
+
 stones = {[{0, 0}, {1, 0}, {2, 0}, {3, 0}],
           [{1, 0}, {1, 2}, {0, 1}, {1, 1}, {2, 1}],
           [{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}],
           [{0, 0}, {0, 1}, {0, 2}, {0, 3}],
-          [{0, 0}, {1, 0}, {0, 1}, {1, 1}]}.each.cycle
-wind = ARGF.gets_to_end.strip.chars.cycle
+          [{0, 0}, {1, 0}, {0, 1}, {1, 1}]}.each.with_index.cycle
+wind = ARGF.gets_to_end.strip.each_char.with_index.cycle
 
 h = 0
+hx = [0] * 7
 field = Array(Array(Bool)).new
 
-2022.times do |i|
-  stone = stones.next.as(Array({Int32, Int32}))
+score1 = nil
+
+cycle_start = 0
+cycle_end = 0
+cycle_heights = [] of Int32
+
+seen = {} of {Array(Int32), Int32} => Int32
+(0..).each do |i|
+  stone, stone_i = stones.next.as({Array({Int32, Int32}), Int32})
   pieces = stone.map { |x, y| {x + 2, y + h + 3 + 1} }
   max_h = pieces.map(&.[1]).max
   while field.size <= max_h
     field << Array.new(7, false)
   end
 
-  # field.each.with_index.to_a.reverse.each do |row, y|
-  #   puts row.each.with_index.map { |f, x|
-  #     if f
-  #       '#'
-  #     elsif pieces.includes?({x, y})
-  #       '@'
-  #     else
-  #       ':'
-  #     end
-  #   }.join
-  # end
-  # puts "*" * 20
-
+  off = nil
   loop do
     nxt_pieces = pieces.map { |x, y| {x, y - 1} }
     break if nxt_pieces.any? { |x, y| y < 0 || field[y][x] }
     pieces = nxt_pieces
 
-    # field.each.with_index.to_a.reverse.each do |row, y|
-    #   puts row.each.with_index.map { |f, x|
-    #     if f
-    #       '#'
-    #     elsif pieces.includes?({x, y})
-    #       '@'
-    #     else
-    #       ':'
-    #     end
-    #   }.join
-    # end
-    # puts "-" * 20
-
-    w = wind.next.as(Char)
+    w, o = wind.next.as({Char, Int32})
+    off ||= o
     d = case w
         when '<' then -1
         when '>' then +1
@@ -73,39 +59,46 @@ field = Array(Array(Bool)).new
 
     nxt_pieces = pieces.map { |x, y| {x + d, y} }
     pieces = nxt_pieces unless nxt_pieces.any? { |x, y| x < 0 || x >= 7 || field[y][x] }
-
-    # field.each.with_index.to_a.reverse.each do |row, y|
-    #   puts row.each.with_index.map { |f, x|
-    #     if f
-    #       '#'
-    #     elsif pieces.includes?({x, y})
-    #       '@'
-    #     else
-    #       ':'
-    #     end
-    #   }.join
-    # end
-    # puts(w.to_s * 20)
   end
 
   pieces.each do |x, y|
     field[y][x] = true
     h = {h, y + 1}.max
+    hx[x] = {hx[x], (y + 1)}.max
   end
 
-  # field.each.with_index.to_a.reverse.each do |row, y|
-  #   puts row.each.with_index.map { |f, x|
-  #     if f
-  #       '#'
-  #     elsif pieces.includes?({x, y})
-  #       '@'
-  #     else
-  #       ':'
-  #     end
-  #   }.join
-  # end
-  # puts "height: #{h}"
-  # puts "=" * 40
+  score1 = h if i + 1 == 2022
+
+  cycle_heights << h
+
+  if stone_i % 5 == 0 && (off = off.not_nil!)
+    hmap = hx.map { |t| h - t }
+    if cycle_start = seen[{hmap, off}]?
+      # puts "Repeat #{cycle_start} -> #{i}  count:#{count_repeated}"
+      if cycle_heights.size >= (i - cycle_start) * 5 && i + 1 >= 2022
+        cycle_end = i
+        break
+      end
+    else
+      cycle_heights.delete_at(1..)
+    end
+    seen[{hmap, off}] = i
+  end
 end
 
-puts "Part 1: #{h}"
+raise "Error" unless cycle_start
+cycle_len = cycle_end - cycle_start
+
+puts "Cycle start: after #{cycle_start} stones (start with stone nr: #{cycle_start + 1})"
+puts "Cycle length: #{cycle_len}"
+
+b = (N - cycle_start - 1) // (cycle_end - cycle_start)
+c = (N - cycle_start - 1) % (cycle_end - cycle_start)
+
+h_beg = cycle_heights[-cycle_len - 1]
+h_end = cycle_heights[-1]
+
+score2 = h_beg.to_i64 + b.to_i64 * (h_end - h_beg) + cycle_heights[-cycle_len - 1 + c] - h_beg
+
+puts "Part 1: #{score1}"
+puts "Part 2: #{score2}"
