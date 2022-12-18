7eade0af1eb3f9d6770f86956b4f781adb61a84e
Frank Fischer <frank-fischer@shadow-soft.de>
2022-12-18 11:01:25 +0100

    18: use arrays instead of sets

--- 18/boilingboulders.cr
+++ 18/boilingboulders.cr
@@ -18,41 +18,45 @@
 cubes = ARGF.each_line.map do |line|
   x, y, z = line.split(',').map(&.to_i)
   {x, y, z}
-end.to_set
+end.to_a
+
+# ensure all cubes are in 0...nx with one layer of air at all boundaries
+minx, maxx = cubes.map(&.[0]).minmax
+miny, maxy = cubes.map(&.[1]).minmax
+minz, maxz = cubes.map(&.[2]).minmax
+nx = maxx - minx + 3
+ny = maxy - miny + 3
+nz = maxz - minz + 3
+cubes.map! { |x, y, z| {x - minx + 1, y - miny + 1, z - minz + 1} }
+
+# 0 = air, 1 = seen-air (outside), 2 = lava
+seen = nx.times.map { ny.times.map { Array.new(nz, 0) }.to_a }.to_a
+cubes.each { |x, y, z| seen[x][y][z] = 2 }
 
 DIRS = { {-1, 0, 0}, {0, -1, 0}, {0, 0, -1}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1} }
 
-def count_edges(cubes)
-  cubes.each.map do |x, y, z|
-    DIRS.each.count do |dx, dy, dz|
-      cubes.includes?({x + dx, y + dy, z + dz})
-    end
-  end.sum // 2
-end
+ncovered = cubes.each.map do |x, y, z|
+  DIRS.each.count do |dx, dy, dz|
+    seen[x + dx][y + dy][z + dz] == 2
+  end
+end.sum
+score1 = cubes.size * 6 - ncovered
 
-score1 = cubes.size * 6 - 2 * count_edges(cubes)
-
-nx = cubes.map(&.[0]).max + 1
-ny = cubes.map(&.[1]).max + 1
-nz = cubes.map(&.[2]).max + 1
-
-# bfs from outside
-u = {-1, -1, -1}
-seen = Set{u}
-q = Deque{u}
+# search from outside
+u = {0, 0, 0}
+q = [u]
 score2 = 0
-while u = q.shift?
+while u = q.pop?
   x, y, z = u
   DIRS.each do |dx, dy, dz|
-    v = {x + dx, y + dy, z + dz}
-    next unless -1 <= v[0] <= nx
-    next unless -1 <= v[1] <= ny
-    next unless -1 <= v[2] <= nz
-    if cubes.includes?(v)
+    vx, vy, vz = x + dx, y + dy, z + dz
+    next unless 0 <= vx < nx && 0 <= vy < ny && 0 <= vz < nz
+    case seen[vx][vy][vz]
+    when 0
+      seen[vx][vy][vz] = 1
+      q << {vx, vy, vz}
+    when 2
       score2 += 1
-    elsif !seen.includes?(v)
-      seen.add(v)
-      q << v
     end
   end
 end
