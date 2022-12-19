c3e18a6cf523140ac9ad6b9ab4a43a29b1651d07
Frank Fischer <frank-fischer@shadow-soft.de>
2022-12-19 12:16:36 +0100

    19: add part 2

--- 19/notenoughminerals.cr
+++ 19/notenoughminerals.cr
@@ -35,69 +35,69 @@ blueprints = ARGF.each_line.map do |line|
   {0, 1, 2, 3}.map { |i| robots[i].not_nil! }
 end.to_a
 
-p blueprints
-
 struct State
   getter minerals : {Int32, Int32, Int32}
-  getter robots : {Int32, Int32, Int32, Int32}
+  getter robots : {Int32, Int32, Int32}
   getter build : Int32?
 
   def initialize(@minerals, @robots, @build = nil); end
 
-  def step : {State, Int32}
+  def step : State
     new_minerals = {0, 1, 2}.map { |i| @minerals[i] + @robots[i] }
     new_robots = @robots.map_with_index { |r, i| i == @build ? r + 1 : r }
-    new_geo = @robots[3]
-    {State.new(new_minerals, new_robots), @robots[3]}
+    State.new(new_minerals, new_robots)
   end
 
   def produce(req : {Int32, Int32, Int32}, robot : Int32) : State?
     if req.zip(@minerals).all? { |req, mn| req <= mn }
       new_minerals = {0, 1, 2}.map { |i| @minerals[i] - req[i] }
-      State.new(new_minerals, @robots, robot)
+      State.new(new_minerals, @robots, robot < 3 ? robot : nil)
     else
       nil
     end
   end
 end
 
-MINUTES = 24
+{ {24, blueprints.size}, {32, 3} }.each_with_index do |params, part|
+  minutes, nblueprints = params
 
-score = 0
-blueprints.each_with_index do |blueprint, bl_idx|
-  puts "========== blueprint: #{bl_idx} =========="
-  best = 0
-  states = {State.new({0, 0, 0}, {1, 0, 0, 0}) => 0}
-  max_robots = {0, 1, 2, 3}.map do |i|
-    i < 3 ? blueprint.map { |reqs| reqs[i] }.max : MINUTES
-  end
-  MINUTES.times do |minute|
-    puts "--- minute: #{minute + 1} ---"
-    nxt_states = {} of State => Int32
-    states.each do |state, val|
-      # p state if minute <= 5
-      # no production
-      nxt, new_geo = state.step
-      new_val = val + new_geo
-      best = {best, new_val}.max
-      if (nxt_states[nxt]? || val - 1) < new_val
-        # puts "    #{nxt.inspect}" if minute <= 5
-        nxt_states[nxt] = new_val
-      end
-      blueprint.each_with_index do |reqs, r|
-        next if nxt.robots[r] >= max_robots[r]
-        if nxt2 = nxt.produce(reqs, r)
-          if (nxt_states[nxt2]? || val - 1) < new_val
-            # puts "    #{nxt2.inspect}" if minute <= 5
-            nxt_states[nxt2] = new_val
+  puts "*** PART #{part + 1} ***"
+  score = part # either 0 or 1
+
+  blueprints.first(nblueprints).each_with_index do |blueprint, bl_idx|
+    puts "========== blueprint: #{bl_idx} =========="
+    best = 0
+    states = {State.new({0, 0, 0}, {1, 0, 0}) => 0}
+    max_robots = {0, 1, 2}.map do |i|
+      i < 3 ? blueprint.map { |reqs| reqs[i] }.max : minutes
+    end
+
+    minutes.times do |minute|
+      puts "--- minute: #{minute + 1} ---"
+      nxt_states = {} of State => Int32
+      states.each do |state, val|
+        # no production
+        nxt = state.step
+        next if val + 4 * (minutes - minute) < best
+        nxt_states[nxt] = val if (nxt_states[nxt]? || val - 1) < val
+        blueprint.each_with_index do |reqs, r|
+          next if r < 3 && nxt.robots[r] >= max_robots[r]
+          if nxt2 = nxt.produce(reqs, r)
+            new_val = val
+            new_val += (minutes - minute - 2) if r == 3 # obsidian robot -> compute produced geodes
+            best = {best, new_val}.max
+            nxt_states[nxt2] = new_val if (nxt_states[nxt2]? || val - 1) < new_val
           end
         end
       end
+      states = nxt_states
     end
-    states = nxt_states
+    if part == 0
+      score += (bl_idx + 1) * best
+    else
+      score *= best
+    end
   end
-  puts "Best: #{best}"
-  score += (bl_idx + 1) * best
-end
 
-puts "Part 1: #{score}"
+  puts "Part #{part + 1}: #{score}"
+end
