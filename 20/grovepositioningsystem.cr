0aba997cb28d1efa5b782cdff3de1e5eac5d8fc0
Frank Fischer <frank-fischer@shadow-soft.de>
2022-12-20 09:47:15 +0100

    20: unify parts 1 and 2

--- 20/grovepositioningsystem.cr
+++ 20/grovepositioningsystem.cr
@@ -16,12 +16,8 @@
 # along with this program.  If not, see  <http://www.gnu.org/licenses/>
 
 indata = ARGF.each_line.map(&.to_i64).with_index.to_a
-indata.map! { |x| {x[0] * 811589153, x[1]} }
-positions = indata.each_with_index.map { |value, pos| {value, pos} }.to_h
-data = indata.clone
 
 def rotate(data, positions, n)
-  return if n[0] == 0
   i = positions[n]
   dir = n[0].sign
   (n[0].abs % (data.size - 1)).abs.times do
@@ -33,19 +29,17 @@ def rotate(data, positions, n)
   positions[n] = i
 end
 
-10.times do |iter|
-  puts iter
-  indata.each do |n|
-    rotate(data, positions, n)
-    raise "Mööp" if indata.any? { |i| data[positions[i]] != i }
+{ {1, 1, 1}, {2, 811589153, 10} }.each do |part, factor, reps|
+  indata.map! { |x| {x[0] * factor, x[1]} }
+  positions = indata.each_with_index.map { |value, pos| {value, pos} }.to_h
+  data = indata.clone
+
+  reps.times do |iter|
+    indata.each { |n| rotate(data, positions, n) }
   end
+
+  zero = positions.find!(&.[0][0].zero?)[1]
+  score = {1000, 2000, 3000}.map { |n| data[(zero + n) % data.size][0] }.sum
+
+  puts "Part #{part}: #{score}"
 end
-
-zero = positions.each_key.find!(&.[0].zero?)
-zero = positions[zero]
-puts zero
-score1 = {1000, 2000, 3000}.map do |n|
-  data[(zero + n) % data.size][0]
-end.sum
-
-puts "Part 1: #{score1}"
